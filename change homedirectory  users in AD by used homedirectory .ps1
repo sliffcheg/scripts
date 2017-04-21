702b535610ab8932683b8d937feaf5aec408a185
AGlinov <alex.glinov@gmail.com>
2017-04-21 14:52:55 +0300

    change homedirectory  users in AD by used homedirectory

---
+++ change homedirectory  users in AD by used homedirectory .ps1
@@ -0,0 +1,5 @@
+﻿Clear-Host
+$users=Get-ADObject -Filter * -Properties homedirectory,SamAccountName,CanonicalName | where {$_.homedirectory -match "bs09msk"} | Select-Object * -ExpandProperty SamAccountName
+foreach ($user in $users){
+Set-ADUser $user.SamAccountName -HomeDirectory \\fs16msk\$user 
+}
